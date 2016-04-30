748eaae7237af13f60ea28373cfb20ceb64736b2
Tony Laidig <laidig@gmail.com>
2016-04-29 22:45:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ ionic start atstop blank
 cd atstop
 ionic setup sass
 git init
-git remote add origin https://github.com/camsys/onebusaway-nyc-atstop.git
+git remote add origin https://github.com/laidig/transitland-atstop.git
 git fetch --all
 git reset --hard origin/master
 rm www/img/ionic.png www/css/style.css .
